c107271efa70b1988446b006612e4d9c858a02af
Husna Zubir <shusnazubir@gmail.com>
2024-01-31 13:00:27 +0800

    enhance the method for DisconnectEventListener

--- chatapp/src/main/java/com/websocket/chatapp/config/WebSocketEventListener.java
+++ chatapp/src/main/java/com/websocket/chatapp/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.websocket.chatapp.config;
 
+import com.websocket.chatapp.model.ChatMessage;
+import com.websocket.chatapp.model.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -10,8 +14,25 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageTemplate;
     @EventListener
     public void handleWebSocketDisconnectEventListener(SessionDisconnectEvent event) {
-        // TODO: implement the logic when user disconnect from the app
+        // when user disconnect from the app
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        // retrieve username from the session
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if(username != null) {
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+
+            // it will send message that the user has left the chat
+            messageTemplate.convertAndSend("/chatroom/public", chatMessage);
+        }
     }
 }
