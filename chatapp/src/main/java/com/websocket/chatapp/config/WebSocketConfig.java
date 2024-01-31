b4d29af1b1530985dd6a951bc878c65014661e29
Husna Zubir <shusnazubir@gmail.com>
2024-01-31 12:04:13 +0800

    create WebSocket configuration

---
+++ chatapp/src/main/java/com/websocket/chatapp/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.websocket.chatapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        endpoint to enter websocket
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+
+//        client can subscribe to messages from public chatroom or private message from user
+        registry.enableSimpleBroker("/chatroom", "/user");
+
+    }
+}
