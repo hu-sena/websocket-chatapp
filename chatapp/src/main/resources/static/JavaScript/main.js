e61949a0e620c24c0c1493edfc81ba384454d0f0
Husna Zubir <shusnazubir@gmail.com>
2024-01-31 17:39:31 +0800

    add WebSocket connection, onConnected + onError

--- chatapp/src/main/resources/static/JavaScript/main.js
+++ chatapp/src/main/resources/static/JavaScript/main.js
@@ -16,3 +16,44 @@ var colors = [
     '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
 ];
 
+function connect(event) {
+//    retrieve username from user input
+    username = document.querySelector('#name').value.trim();
+
+//    when username is true, usernamePage = hidden, chatPage = visible
+    if(username) {
+        usernamePage.classList.add('hidden');
+        chatPage.classList.remove('hidden');
+
+        var socket = new SockJS('/ws');
+        stompClient = Stomp.over(socket);
+
+        stompClient.connect({}, onConnected, onError);
+    }
+
+//    when user is not true, stop default behaviour
+    event.preventDefault();
+}
+
+
+function onConnected() {
+    // subscribe to the chatroom
+    stompClient.subscribe('/chatroom/public', onMessageReceived);
+
+    // add username to server
+    stompClient.send("/app/chat.addUser", {},
+        JSON.stringify({
+            sender: username,
+            type: 'JOIN'
+        })
+    )
+
+//    when successfully connected (hide: connecting...)
+    connectingElement.classList.add('hidden');
+}
+
+
+function onError(error) {
+    connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
+    connectingElement.style.color = 'red';
+}
