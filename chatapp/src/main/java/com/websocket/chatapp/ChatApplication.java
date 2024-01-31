5829564649e10877d35f5d73e965e4eed1832414
Husna Zubir <shusnazubir@gmail.com>
2024-01-31 12:03:41 +0800

    rename main file to ChatApplication

--- chatapp/src/main/java/com/websocket/chatapp/ChatApplication.java
+++ chatapp/src/main/java/com/websocket/chatapp/ChatApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatappApplication {
+public class ChatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatappApplication.class, args);
+		SpringApplication.run(ChatApplication.class, args);
 	}
 
 }
